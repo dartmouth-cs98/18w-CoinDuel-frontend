33692a1e623c8f3f51f3a53faacb6ea66bc1607c
Henry Wilson <henryhwilson@gmail.com>
2018-02-11 13:18:51 -0500

    Added view controllers for Leaderborad and Game view

---
+++ CoinDuel/CoinDuel/GameViewController.swift
@@ -0,0 +1,47 @@
+//
+//  GameViewController.swift
+//  CoinDuel
+//
+//  Created by Henry Wilson on 1/25/18.
+//  Copyright © 2018 Capitalize. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var ViewLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var serverMessage: String?
+        let apiUrl = NSURL(string:"https://coinduel-cs98.herokuapp.com/");
+        
+        let request = NSMutableURLRequest(url:apiUrl! as URL);
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print("error connecting to server")
+                return
+            }
+            
+            let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            serverMessage = "\(responseString!)"
+            DispatchQueue.main.async() {
+                self.ViewLabel.text = serverMessage
+            }
+        }
+        
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
