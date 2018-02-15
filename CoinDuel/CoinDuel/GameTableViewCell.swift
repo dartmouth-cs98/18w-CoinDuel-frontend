d8b3bb07ece3a2540158b37f3c2835bcac5981d0
Henry Wilson <henryhwilson@gmail.com>
2018-02-15 18:09:46 -0500

    Added list of currencies with flip switch and submit button

---
+++ CoinDuel/CoinDuel/GameTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  UserTableViewCell.swift
+//  CoinDuel
+//
+//  Created by Mitchell Revers on 2/11/18.
+//  Copyright © 2018 Capitalize. All rights reserved.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+    @IBOutlet weak var choiceSwitch: UISwitch!
+    @IBOutlet weak var coinNameLabel: UILabel!
+    @IBOutlet weak var coinPriceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
--- CoinDuel/CoinDuel/GameViewController.swift
+++ CoinDuel/CoinDuel/GameViewController.swift
@@ -8,16 +8,39 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        scoreLabel.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    let coins = ["Bitcoin", "Litecoin", "Ethereum", "Monero", "Ripple", "Bitcoin Cash"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return coins.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "GameTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? GameTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of GameTableViewCell.")
+        }
+        
+        let coin = coins[indexPath.row]
+        cell.coinNameLabel.text = coin
+        cell.coinPriceLabel.text = "1000"
+        
+        cell.coinPriceLabel.isHidden = true
+        
+        return cell
     }
 
 
