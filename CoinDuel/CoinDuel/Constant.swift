5ffd4dd510d75879a666dd604950cd40a49514df
Henry Wilson <henryhwilson@gmail.com>
2018-02-15 21:05:18 -0500

    Added constants file

---
+++ CoinDuel/CoinDuel/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  CoinDuel
+//
+//  Created by Henry Wilson on 2/15/18.
+//  Copyright © 2018 Capitalize. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let API = "https://coinduel-cs98.herokuapp.com/"
+}
+
+
