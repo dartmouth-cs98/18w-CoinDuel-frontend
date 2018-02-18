f6f3608bb9fa52ab42cd01acbad407c9e607671c
Mitchell Revers <mitchell.a.revers.18@dartmouth.edu>
2018-02-18 14:42:08 -0500

    login now is functional

--- CoinDuel/Login/SignInViewController.swift
+++ CoinDuel/Login/SignInViewController.swift
@@ -12,9 +12,13 @@ class SignInViewController: UIViewController {
 
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var failedLogin: UILabel!
+    
+    var validated = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        failedLogin.isHidden = true
         // Do any additional setup after loading the view.
     }
 
@@ -24,9 +28,45 @@ class SignInViewController: UIViewController {
     }
     
     @IBAction func onLoginPressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "GameViewController") as UIViewController
-        present(vc, animated: true, completion: nil)
+        if let user = username.text, !user.isEmpty, let pass = password.text, !pass.isEmpty {
+            let apiUrl = NSURL(string:Constants.API + "user");
+            let request = NSMutableURLRequest(url:apiUrl! as URL);
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                
+                if error != nil {
+                    print("error connecting to server")
+                    return
+                }
+                
+                if let json = try? JSONSerialization.jsonObject(with: data!, options: []) {
+                    if let response = json as? NSArray {
+                        for key in response {
+                            if let dict = key as? NSDictionary {
+                                if let name = dict.value(forKey: "username") as? String, let pw = dict.value(forKey: "password") as? String {
+                                    if user == name, pass == pw {
+                                        self.validated = true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                DispatchQueue.main.async() {
+                    if (self.validated) {
+                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                        let vc = storyboard.instantiateViewController(withIdentifier: "GameViewController") as UIViewController
+                        self.present(vc, animated: true, completion: nil)
+                    }
+                    else {
+                        self.failedLogin.isHidden = false
+                    }
+                }
+            }
+            
+            task.resume()
+        }
     }
 
     /*
