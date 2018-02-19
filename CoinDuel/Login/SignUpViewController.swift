8b3b75390db29d35d47e3241a5d78c77d7974fff
RajivRamaiah <rajivramaiah@me.com>
2018-02-18 20:50:33 -0500

    finished sign up processes

--- CoinDuel/Login/SignUpViewController.swift
+++ CoinDuel/Login/SignUpViewController.swift
@@ -15,10 +15,16 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var confirmedPassword: UITextField!
 
+    @IBOutlet weak var tintView: UIView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var noPasswordLabel: UILabel!
+    @IBOutlet weak var noUsernameLabel: UILabel!
+    @IBOutlet weak var noEmailLabel: UILabel!
+
+    var validated = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -29,19 +35,61 @@ class SignUpViewController: UIViewController {
 
 
     @IBAction func onSignUpPressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "GameViewController") as UIViewController
-        present(vc, animated: true, completion: nil)
+
+//        display error messages if fields are empty
+        if (username.text == "" || email.text == "" || password.text == "" || confirmedPassword.text == ""){
+            let alert = UIAlertController(title: "Invalid Parameters", message: "Please fill in all appropriate fields!", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Go Back", style: .default) { (action:UIAlertAction) in
+            }
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else if (password.text != confirmedPassword.text){
+            let alert = UIAlertController(title: "Incorrect Passwords", message: "Please make sure your passwords match!", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Go Back", style: .default) { (action:UIAlertAction) in
+            }
+            
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+//        if all field are filled sign up the user
+        else{
+            self.activityIndicator.isHidden = false
+            self.activityIndicator.startAnimating()
+            let params = ["username": username.text, "email": email.text, "password": password.text]
+
+            let apiUrl = URL(string: Constants.API + "signup")
+            var request = URLRequest(url:apiUrl! as URL);
+
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+            guard let body = try? JSONSerialization.data(withJSONObject: params, options: []) else{
+                print("error making body"); return
+            }
+
+            request.httpBody = body
+
+            let session = URLSession.shared
+            session.dataTask(with: request, completionHandler: { (data, response, error) in
+                if let httpResponse = response as? HTTPURLResponse {
+                    if (httpResponse.statusCode == 200){
+                        self.validated = true
+                        DispatchQueue.main.async {
+                            self.activityIndicator.isHidden = true
+                            self.activityIndicator.stopAnimating()
+                            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                            let vc = storyboard.instantiateViewController(withIdentifier: "GameViewController") as UIViewController
+                            self.present(vc, animated: true, completion: nil)
+                        }
+                    }
+                }
+
+                if let error = error {
+                    print(error)
+                }
+            }).resume()
+        }
+
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
