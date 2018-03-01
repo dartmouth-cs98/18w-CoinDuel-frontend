9f73130307673005d1406ccd915c7d0577b5567f
Henry Wilson <henryhwilson@gmail.com>
2018-03-01 17:52:30 -0500

    leaderboard all time works

--- CoinDuel/CoinDuel/Leaderboard.swift
+++ CoinDuel/CoinDuel/Leaderboard.swift
@@ -21,6 +21,8 @@ class Leaderboard {
     }
         
     func getCurrentLeaderboard(_ leaderboardVC:LeaderboardViewController) {
+        self.users = []
+        
         self.game.getCurrentGame() { (success) -> Void in
             if success {
                 let url = URL(string: Constants.API + "leaderboard/" + self.game.id)!
@@ -40,14 +42,64 @@ class Leaderboard {
                             leaderboardVC.firstPlaceLabel.text = self.users[0].username
                             leaderboardVC.secondPlaceLabel.text = self.users[1].username
                             leaderboardVC.thirdPlaceLabel.text = self.users[2].username
+                            
+                            leaderboardVC.refreshControl.endRefreshing()
+                            leaderboardVC.loadingActivityIndicatorView.stopAnimating()
                         }
                     case .failure(let error):
+                        leaderboardVC.leaderboardTable.reloadData()
+                        leaderboardVC.firstPlaceLabel.text = ""
+                        leaderboardVC.secondPlaceLabel.text = ""
+                        leaderboardVC.thirdPlaceLabel.text = ""
+                        
+                        leaderboardVC.refreshControl.endRefreshing()
+                        leaderboardVC.loadingActivityIndicatorView.stopAnimating()
+                        
                         print(error)
+                        leaderboardVC.networkError("Could not retrieve the leaderboard for this game")
                     }
                 }
             } else {
-                
+                leaderboardVC.allTimeClick(self)
             }
+        }
+    }
+    
+    func getAllTimeLeaderboard(_ leaderboardVC:LeaderboardViewController) {
+        self.users = []
+        
+        let url = URL(string: Constants.API + "leaderboard")!
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+                case .success(let value):
+                    let jsonArray = JSON(value).arrayValue
+                    for obj in jsonArray {
+                        let name = obj["userId"]["username"].stringValue
+                        let coins = obj["coin_balance"].doubleValue
+                        self.users.append(User(username: name, coinBalance: coins))
+                    }
+                    self.users = self.users.sorted(by: { $0.coinBalance > $1.coinBalance })
+                    
+                    DispatchQueue.main.async() {
+                        leaderboardVC.leaderboardTable.reloadData()
+                        leaderboardVC.firstPlaceLabel.text = self.users[0].username
+                        leaderboardVC.secondPlaceLabel.text = self.users[1].username
+                        leaderboardVC.thirdPlaceLabel.text = self.users[2].username
+                        
+                        leaderboardVC.refreshControl.endRefreshing()
+                        leaderboardVC.loadingActivityIndicatorView.stopAnimating()
+                    }
+                case .failure(let error):
+                    leaderboardVC.leaderboardTable.reloadData()
+                    leaderboardVC.firstPlaceLabel.text = ""
+                    leaderboardVC.secondPlaceLabel.text = ""
+                    leaderboardVC.thirdPlaceLabel.text = ""
+                    
+                    leaderboardVC.refreshControl.endRefreshing()
+                    leaderboardVC.loadingActivityIndicatorView.stopAnimating()
+                    print(error)
+                    leaderboardVC.networkError("Could not retrieve the leaderboard for all time")
+                }
         }
     }
 }
