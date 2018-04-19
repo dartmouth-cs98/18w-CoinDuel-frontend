b03a5ea75ede8ade05a7b069449316f822b054fb
Anish Chadalavada <anish.chada@gmail.com>
2018-04-19 18:33:06 -0400

    UI color scheme changes

--- CoinDuel/CoinDuel/Controllers/LoginViewController.swift
+++ CoinDuel/CoinDuel/Controllers/LoginViewController.swift
@@ -9,10 +9,28 @@
 import Foundation
 import UIKit
 
+extension UIView {
+    func applyGradient(colours: [UIColor]) -> Void {
+        self.applyGradient(colours: colours, locations: nil)
+    }
+    
+    func applyGradient(colours: [UIColor], locations: [NSNumber]?) -> Void {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+}
+
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var signinButton: UIButton!
     @IBOutlet weak var signupButton: UIButton!
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var gifView: UIImageView!
     override func viewDidLoad() {
@@ -20,6 +38,9 @@ class LoginViewController: UIViewController {
         signinButton.layer.cornerRadius = 5
         signupButton.layer.masksToBounds = true
         signupButton.layer.cornerRadius = 5
+        
+        // apply gradient to screen
+        self.imageView.applyGradient(colours: [UIColor(red:0.54, green:0.15, blue:0.31, alpha:1.0), UIColor(red:0.65, green:0.18, blue:0.38, alpha:1.0)])
         
         gifView.loadGif(asset: "colors1")
     }
