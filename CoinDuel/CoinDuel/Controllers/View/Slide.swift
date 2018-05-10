dd94aeb04733b0211d339cc33f93d76891fef0fa
Anish Chadalavada <anish.chada@gmail.com>
2018-05-10 16:51:15 -0400

    creating onboarding sliders

---
+++ CoinDuel/CoinDuel/Controllers/View/Slide.swift
@@ -0,0 +1,21 @@
+//
+//  Slide.swift
+//  CoinDuel
+//
+//  Created by Anish Chadalavada on 5/10/18.
+//  Copyright © 2018 Capitalize. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
