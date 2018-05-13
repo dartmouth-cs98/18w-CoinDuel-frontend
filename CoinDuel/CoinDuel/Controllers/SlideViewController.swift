f96482d60f1923d73206e7df1d7bc6870f35411f
Anish Chadalavada <anish.chada@gmail.com>
2018-05-13 15:56:26 -0400

    fixed several bugs with sliders

--- CoinDuel/CoinDuel/Controllers/SlideViewController.swift
+++ CoinDuel/CoinDuel/Controllers/SlideViewController.swift
@@ -2,39 +2,34 @@
 //  SlideViewController.swift
 //  CoinDuel
 //
-//  Created by Anish Chadalavada on 5/10/18.
+//  Created by Anish Chadalavada on 5/13/18.
 //  Copyright © 2018 Capitalize. All rights reserved.
 //
 
 import UIKit
 
-// This code is also based on this tutorial
-
-// https://www.youtube.com/watch?v=Vq71SPkJIus
+// Below code from tutorial at
+// https://www.youtube.com/watch?v=X2Wr4TtMG6Q
 
 class SlideViewController: UIViewController {
-    
-    
-    @IBOutlet weak var slideScrollView: UIScrollView!
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for image in 0...2 {
+            let image2 = String(image)
+            let imageToDisplay = UIImage(named: image2)
+            print(type(of: image))
+            let imageView = UIImageView(image: imageToDisplay)
+            let xCoordinate = view.frame.midX + view.frame.width * CGFloat(image)
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: xCoordinate - 50, y: (view.frame.height / 2) - 50, width: 256, height: 256)
+        }
 
         // Do any additional setup after loading the view.
- 
-    }
-    
-    // 2 functions below based on tutorial at https://www.youtube.com/watch?v=Vq71SPkJIus
-    
-    func createSlides() -> [Slide] {
-        
-        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: Slide, options: nil)
-        
-        return []
-    }
-    
-    func setupSlideScrollView(slides:[Slide]) {
-        
     }
 
     override func didReceiveMemoryWarning() {
