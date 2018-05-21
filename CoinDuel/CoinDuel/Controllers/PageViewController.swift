6ab4430dd4cee8fb2bd903018ca492501cc594d1
Anish Chadalavada <anish.chada@gmail.com>
2018-05-21 16:49:22 -0400

    page view scroll working better than other scroll view now

--- CoinDuel/CoinDuel/Controllers/PageViewController.swift
+++ CoinDuel/CoinDuel/Controllers/PageViewController.swift
@@ -10,15 +10,6 @@ import UIKit
 
 class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        <#code#>
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        <#code#>
-    }
-    
-    
     lazy var orderedViewControllers: [UIViewController] = {
         return [self.newVc(viewController: "sbRed"),
                 self.newVc(viewController: "sbBlue")]
@@ -26,14 +17,55 @@ class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UI
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.dataSource = self
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
 
         // Do any additional setup after loading the view.
     }
     
     func newVc(viewController: String) -> UIViewController {
-        return UIStoryboard(name: "login", bundle: nil).instantiateViewController(withIdentifier:viewController)
+        return UIStoryboard(name: "Login", bundle: nil).instantiateViewController(withIdentifier:viewController)
     }
-
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard orderedViewControllers.count != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllers.count > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
