9c169d1ba75d16a73b04fcb5e05c57e378e469bc
Anish Chadalavada <anish.chada@gmail.com>
2018-05-24 18:06:45 -0400

    working pageviewcontroller and pagecontrol

---
+++ CoinDuel/CoinDuel/Controllers/OnboardingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  OnboardingViewController.swift
+//  CoinDuel
+//
+//  Created by Anish Chadalavada on 5/24/18.
+//  Copyright © 2018 Capitalize. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    override func viewDidLayoutSubviews(){
+        self.backgroundImageView.applyGradient(colours: [UIColor(red:0.43, green:0.29, blue:0.63, alpha:1.0), UIColor(red:0.18, green:0.47, blue:0.75, alpha:1.0)])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
