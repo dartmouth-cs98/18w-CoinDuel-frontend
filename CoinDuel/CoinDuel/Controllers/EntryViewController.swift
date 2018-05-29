c6ced8de5d60375f1a7cecfa4f35dd26402ad9fb
Anish Chadalavada <anish.chada@gmail.com>
2018-05-29 16:59:46 -0400

    storyboard entry point working

---
+++ CoinDuel/CoinDuel/Controllers/EntryViewController.swift
@@ -0,0 +1,40 @@
+//
+//  EntryViewController.swift
+//  CoinDuel
+//
+//  Created by Anish Chadalavada on 5/29/18.
+//  Copyright © 2018 Capitalize. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
