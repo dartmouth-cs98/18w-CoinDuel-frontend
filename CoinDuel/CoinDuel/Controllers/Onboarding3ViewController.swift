85eccdddfe65337a4c88b629b2929a62162e696b
Anish Chadalavada <anish.chada@gmail.com>
2018-05-28 21:03:11 -0400

    logo animation working at last

--- CoinDuel/CoinDuel/Controllers/Onboarding3ViewController.swift
+++ CoinDuel/CoinDuel/Controllers/Onboarding3ViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class Onboarding3ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var sampleConstraint: NSLayoutConstraint!
+    
+    // Color scheme based on instagram and https://stackoverflow.com/questions/47800574/gradient-over-instagram-svg-of-fontawesome-5
+    
+    override func viewDidLayoutSubviews(){
+        self.imageView.applyGradient(colours: [UIColor(red:0.43, green:0.29, blue:0.63, alpha:1.0), UIColor(red:0.18, green:0.47, blue:0.75, alpha:1.0)])
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
         // Do any additional setup after loading the view.
     }
+    
+    // https://stackoverflow.com/questions/28127259/update-the-constant-property-of-a-constraint-programmatically-in-swift
     
     override func viewDidAppear(_ animated: Bool) {
         self.view.layoutIfNeeded()
